1b4e2a1a2e5cd5ef1d5dc6f38069f59ffac41e36
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-03 20:24:41 -0300

    using context/hook

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,14 +1,16 @@
 import { Text, Center, Icon } from "native-base";
 import { Button } from "../components/Button";
 import { Fontisto } from "@expo/vector-icons";
+import { useAuth } from "../hooks/useAuth";
 
 import Logo from "../assets/logo.svg";
 
 export function SignIn() {
+    const { signIn } = useAuth();
     return (
         <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40} />
-            <Button title="ENTRAR COM GOOGLE" type="SECONDARY" mt={12}
+            <Button title="ENTRAR COM GOOGLE" type="SECONDARY" mt={12} onPress={signIn}
                 leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />} />
 
             <Text color="white" textAlign="center" mt={5}>
