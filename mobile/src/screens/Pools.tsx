594d30bb7a6ff29d93112570ae768eb4fbf39fe6
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-05 18:23:18 -0300

    add Navigation in Pools

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -2,13 +2,17 @@ import { VStack, Icon } from "native-base";
 import { Header } from './../components/Header';
 import { Button } from './../components/Button';
 import { Octicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 export function Pools() {
+    const { navigate } = useNavigation();
+
     return (
         <VStack flex={1} bgColor="gray.900">
             <Header title="Meus bolões" />
             <VStack mt={6} mx={5} borderBottomColor="gray.600" borderBottomWidth={1} mb={4} pb={4} >
-                <Button leftIcon={<Icon as={Octicons} name="search" color="black" size="md"/>} title="BUSCAR BOLÃO POR OÓDIGO" />
+                <Button leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />} title="BUSCAR BOLÃO POR OÓDIGO"
+                    onPress={() => navigate("find")} />
             </VStack>
         </VStack>
     );
