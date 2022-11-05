4351253358c965c2b05008d1daab088fd6eab52a
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-05 19:00:00 -0300

    fix signin loading

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -6,12 +6,12 @@ import { useAuth } from "../hooks/useAuth";
 import Logo from "../assets/logo.svg";
 
 export function SignIn() {
-    const { signIn } = useAuth();
+    const { signIn, isUserLoading } = useAuth();
     return (
         <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40} />
-            <Button title="ENTRAR COM GOOGLE" type="SECONDARY" mt={12} onPress={signIn}
-                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />} />
+            <Button title="ENTRAR COM GOOGLE" type="SECONDARY" mt={12} onPress={signIn} isLoading={isUserLoading}
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />} _loading={{ _spinner: { color: "white" } }} />
 
             <Text color="white" textAlign="center" mt={5}>
                 Não iremos utilizar nenhuma informação alem {"\n"}do seu e-mail para a criação de sua conta.
