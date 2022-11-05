aa1562c85d73cc39674734ce440d3c5ec8842169
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-05 19:00:24 -0300

    create login with google

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -2,7 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import * as Google from "expo-auth-session/providers/google";
 import * as AuthSession from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
-
+import { api } from './../services/apit';
 
 WebBrowser.maybeCompleteAuthSession();
 interface UserProps {
@@ -45,11 +45,26 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
     }
 
     async function signInWithGoogle(access_token: string) {
-        console.log("token auth", access_token);
+        try {
+            setUserLoading(true);
+
+            const tokenResponse = await api.post("users", { access_token });
+
+            api.defaults.headers.common["Authorization"] = `Bearer ${tokenResponse.data.token}`;
+
+            const userInfoResponse = await api.get("/me");
+            setUser(userInfoResponse.data.user);
+        }
+        catch (ex) {
+            throw ex;
+        }
+        finally {
+            setUserLoading(false);
+        }
     }
 
     useEffect(() => {
-        if (response.type === "success" && response?.authentication?.accessToken) {
+        if (response?.type === "success" && response?.authentication?.accessToken) {
             signInWithGoogle(response.authentication.accessToken);
         }
     }, [response]);
