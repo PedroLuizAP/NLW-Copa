8ccdb6d69b401281dabb7f0dc637460951d7e824
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-06 10:59:52 -0300

    add server to create poll

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,10 +1,51 @@
-import { Heading, VStack, Text } from "native-base";
+import { Heading, VStack, Text, useToast } from "native-base";
 import { Header } from "../components/Header";
 import Logo from "../assets/logo.svg";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useState } from 'react';
+import { api } from "../services/api";
 
 export function New() {
+    const [title, setTitle] = useState("");
+    const toast = useToast();
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function handlePollCreate() {
+        if (!title.trim()) {
+            return toast.show({
+                title: "O nome do bolão não pode ser vazio.",
+                placement: "top",
+                bgColor: "red.500"
+            });
+        }
+        try {
+            setIsLoading(true);
+
+            await api.post("polls", {title});
+
+            toast.show({
+                title: "Bolão criado com sucesso.",
+                placement: "top",
+                bgColor: "green.500"
+            });
+
+            setTitle("");
+        }
+        catch (ex) {
+            toast.show({
+                title: "Não foi possivel criar o bolão.",
+                placement: "top",
+                bgColor: "red.500"
+            });
+            
+        }
+        finally {
+            setIsLoading(false);
+
+        }
+    }
+
     return (
         <VStack flex={1} bgColor="gray.900">
             <Header title="Criar novo bolão" />
@@ -16,12 +57,12 @@ export function New() {
                     Crie seu próprio bolão da copa {"\n"} e compatilhe entre amigos.
                 </Heading>
 
-                <Input mb={2} placeholder="Qual o nome do seu bolão?"/>
+                <Input mb={2} placeholder="Qual o nome do seu bolão?" onChangeText={setTitle} value={title} />
 
-                <Button title="CRIAR MEU BOLÃO"/>
+                <Button title="CRIAR MEU BOLÃO" onPress={handlePollCreate} isLoading={isLoading} />
 
                 <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
-                Após criar seu bolão você receberá um código único que poderá usar para convidar seus amigos 🚀
+                    Após criar seu bolão você receberá um código único que poderá usar para convidar seus amigos 🚀
 
                 </Text>
             </VStack>
